9ba504f4825bccab82a5acb3befe80091ce11802
Andrew Alexandroff <shurochkin@gmail.com>
2023-03-14 13:33:54 +0300

    add new project

---
+++ precache-manifest.e9e19f37536621d706c58cc03bb65512.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "594b213922e6322911ba",
+    "url": "css/app.a06ecc4e.css"
+  },
+  {
+    "revision": "594b213922e6322911ba",
+    "url": "js/app.f92aef36.js"
+  },
+  {
+    "revision": "528ca498edbe13f9e34e",
+    "url": "js/chunk-vendors.e930dcfa.js"
+  },
+  {
+    "revision": "cc2ccf92cb4667d09da4a115d7de2ba2",
+    "url": "index.html"
+  },
+  {
+    "revision": "ee875af15e7691cb99bee97baf3d20df",
+    "url": "data/files/convert.docx"
+  },
+  {
+    "revision": "af756fa13554220e85d2f1ba73ad1cbc",
+    "url": "data/documents.json"
+  },
+  {
+    "revision": "5e084ad4cf595e0594d5071d067e728a",
+    "url": "data/files/document.doc"
+  }
+];
